56086ef8b7d188f1680712adcd740e9844ac0a27
GhosTHaise <Ghostrex2gmail.com>
2022-12-01 21:35:14 +0300

    First commit

---
+++ app/src/main/java/mg/ghostcorp/medicalapp/MainActivity.java
@@ -0,0 +1,18 @@
+package mg.ghostcorp.medicalapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
