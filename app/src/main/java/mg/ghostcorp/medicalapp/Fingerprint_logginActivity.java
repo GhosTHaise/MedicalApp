d53aa22696fc734cac50d66dc9c0fd439e225d2f
GhosTHaise <Ghostrex2gmail.com>
2022-12-02 18:09:33 +0300

    add lottie to more view effect

--- app/src/main/java/mg/ghostcorp/medicalapp/Fingerprint_logginActivity.java
+++ app/src/main/java/mg/ghostcorp/medicalapp/Fingerprint_logginActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
 
+import com.airbnb.lottie.LottieAnimationView;
+
 public class Fingerprint_logginActivity extends AppCompatActivity {
 
     @Override
@@ -13,5 +15,11 @@ public class Fingerprint_logginActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fingerprint_loggin);
+
+        LottieAnimationView fingerprint = findViewById(R.id.finger_btn);
+
+        fingerprint.setOnClickListener(v -> {
+            fingerprint.playAnimation();
+        });
     }
 }
