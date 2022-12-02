4c4c1530b5cc5113024f5b9143b637a89351957f
GhosTHaise <Ghostrex2gmail.com>
2022-12-02 13:20:25 +0300

    add connexion btn ressources and insertion

---
+++ app/src/main/java/mg/ghostcorp/medicalapp/Fingerprint_logginActivity.java
@@ -0,0 +1,14 @@
+package mg.ghostcorp.medicalapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Fingerprint_logginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fingerprint_loggin);
+    }
+}
--- app/src/main/java/mg/ghostcorp/medicalapp/MainActivity.java
+++ app/src/main/java/mg/ghostcorp/medicalapp/MainActivity.java
@@ -2,15 +2,28 @@ package mg.ghostcorp.medicalapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView connexion_btn,registration_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+        //Full screen window
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        connexion_btn = findViewById(R.id.home_connexion);
+        registration_btn =  findViewById(R.id.home_registration);
+
+        connexion_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent
+            }
+        });
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
