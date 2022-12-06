17b71270aa105c3230efa5372c377ba2056dea6f
GhosTHaise <Ghostrex2gmail.com>
2022-12-06 19:57:43 +0300

    add slide Motion event

--- app/src/main/java/mg/ghostcorp/medicalapp/Fingerprint_logginActivity.java
+++ app/src/main/java/mg/ghostcorp/medicalapp/Fingerprint_logginActivity.java
@@ -1,14 +1,17 @@
 package mg.ghostcorp.medicalapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.WindowManager;
 
 import com.airbnb.lottie.LottieAnimationView;
 
 public class Fingerprint_logginActivity extends AppCompatActivity {
-
+    float x1,x2,y1,y2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -21,5 +24,22 @@ public class Fingerprint_logginActivity extends AppCompatActivity {
         fingerprint.setOnClickListener(v -> {
             fingerprint.playAnimation();
         });
+    }
+
+    public boolean onTouchEvent(@NonNull MotionEvent touchEvent){
+        switch (touchEvent.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                x1 = touchEvent.getX();
+                y1 = touchEvent.getY();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = touchEvent.getX();
+                y2 = touchEvent.getY();
+                if(x1 > x2){
+                    startActivity(new Intent(this,Fingerprint_logginActivity.class));
+                }
+                break;
+        }
+        return false;
     }
 }
--- app/src/main/java/mg/ghostcorp/medicalapp/RegistrationActivity.java
+++ app/src/main/java/mg/ghostcorp/medicalapp/RegistrationActivity.java
@@ -1,16 +1,36 @@
 package mg.ghostcorp.medicalapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.WindowManager;
 
 public class RegistrationActivity extends AppCompatActivity {
-
+    float x1,x2,y1,y2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+    }
+
+    public boolean onTouchEvent(@NonNull MotionEvent touchEvent){
+        switch (touchEvent.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                x1 = touchEvent.getX();
+                y1 = touchEvent.getY();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = touchEvent.getX();
+                y2 = touchEvent.getY();
+                if(x1 < x2){
+                    startActivity(new Intent(this,Fingerprint_logginActivity.class));
+                }
+                break;
+        }
+        return false;
     }
 }
